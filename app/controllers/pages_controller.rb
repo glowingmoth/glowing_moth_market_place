8b2e95126874922e2817d8fca685ae887f118495
Thomas Doig <thomasdoig@hotmail.com>
2020-07-27 16:13:54 +1000

    added navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      redirect_to assets_path
+    else
+      redirect_to new_user_registration_path
+    end
   end
 
   def about
