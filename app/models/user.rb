99196c23ae23f4d777035928a149f21c28c40a1b
Thomas Doig <thomasdoig@hotmail.com>
2020-08-03 16:30:15 +1000

    added rolify gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,6 +7,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :purchases
-  has_many :assets
+  has_many :assets, dependent: :destroy
   validates :name, presence: true
   end
