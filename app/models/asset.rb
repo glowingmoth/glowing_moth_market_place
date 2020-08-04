9b6c6343fceaee570010fd5050ae65da40c2b79e
Thomas Doig <thomasdoig@hotmail.com>
2020-08-04 11:10:11 +1000

    fixed a typo in purchase.rb and created a purchase method within asset.rb

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,10 @@
 class Asset < ApplicationRecord
     belongs_to :user
-    has_many :purchases
+    has_many :purchases, dependent: :destroy
     has_many_attached :pictures
     has_one_attached :zip_file
+
+    def has_purchased?(user_id)
+        self.purchases.find_by_user_id(user_id)
+    end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,4 @@
 class Purchase < ApplicationRecord
   belongs_to :user
-  belongs_to :assets
+  belongs_to :asset
 end
